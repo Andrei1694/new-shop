effa06fffeeacdee03becb6ec57b6b3835b98150
Andrei1694 <andrei4.stanciu@gmail.com>
2024-11-26 17:28:27 +0200

    added dockerfile

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const products = [
 	{ id: 1, name: "Farm Fresh Eggs", price: 4.99, category: "Dairy", image: 'https://picsum.photos/200/300', description: "Fresh eggs from free-range chickens, perfect for your breakfast or baking needs." },
 	{ id: 2, name: "Organic Honey", price: 8.50, category: "Sweeteners", image: 'https://picsum.photos/200/300', description: "Pure, unprocessed honey from local beekeepers. A natural sweetener for your tea or toast." },
-	{ id: 3, name: "Artisan Bread", price: 5.99, category: "Bakery", image: 'https://picsum.photos/200/300', description: "Freshly baked artisan bread, made with organic flour and traditional methods." },
+	{ id: 3, name: "Artisan Breadzz", price: 5.99, category: "Bakery", image: 'https://picsum.photos/200/300', description: "Freshly baked artisan bread, made with organic flour and traditional methods." },
 	{ id: 4, name: "Fresh Spinach", price: 3.99, category: "Vegetables", image: 'https://picsum.photos/200/300', description: "Crisp, nutrient-rich spinach leaves, ideal for salads or cooking." },
 	{ id: 5, name: "Grass-fed Beef", price: 12.99, category: "Meat", image: 'https://picsum.photos/200/300', description: "Premium grass-fed beef, raised without antibiotics or hormones." },
 	{ id: 6, name: "Organic Apples", price: 6.99, category: "Fruits", image: 'https://picsum.photos/200/300', description: "Sweet and crisp organic apples, perfect for snacking or baking." },
